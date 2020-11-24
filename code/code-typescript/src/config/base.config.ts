cc655ce331d3b4aab7dbc599e7b4142f709bacab
Andrey Linko <AndreyLinko@gmail.com>
2020-11-24 11:20:34 +0300

    feat(common): version

--- code/code-typescript/src/config/base.config.ts
+++ code/code-typescript/src/config/base.config.ts
@@ -36,5 +36,11 @@ export default {
 
     outDir: './dist',
   },
-  exclude: ['node_modules', 'src/**/*.spec.ts', 'src/**/*.test.ts', '**/*/dist/**/*.d.ts'],
+  exclude: [
+    'node_modules',
+    'src/**/*.spec.ts',
+    'src/**/*.test.ts',
+    '**/*/dist/**/*.d.ts',
+    'integration/**/*.test.ts',
+  ],
 }
