904dcc4ac60261d74185211b3b2e5cf6b5f847c4
Andrey Linko <AndreyLinko@gmail.com>
2023-04-08 02:01:13 +0300

    fix: runtime and typescript injection

--- yarn/plugin-runtime/src/validate-project.hook.ts
+++ yarn/plugin-runtime/src/validate-project.hook.ts
@@ -1,16 +1,29 @@
 import type { Project } from '@yarnpkg/core'
 
+import { structUtils }  from '@yarnpkg/core'
+
 import semver           from 'semver'
 
 import runtime          from '@monstrs/code-runtime/package.json' assert { type: 'json' }
 
 export const validateProject = async (project: Project) => {
   if (project.topLevelWorkspace.manifest.raw.devDependencies) {
-    const exist = project.topLevelWorkspace.manifest.raw.devDependencies[runtime.name]
+    const ident = structUtils.parseIdent(runtime.name)
 
-    if (!exist || (semver.valid(exist) && semver.lt(exist, runtime.version))) {
-      // eslint-disable-next-line no-param-reassign
-      project.topLevelWorkspace.manifest.raw.devDependencies[runtime.name] = `^${runtime.version}`
+    let descriptor = Array.from(project.topLevelWorkspace.manifest.devDependencies.values()).find(
+      (target) => target.scope === ident.scope && target.name === ident.name
+    )
+
+    if (!descriptor) {
+      descriptor = structUtils.makeDescriptor(ident, `^${runtime.version}`)
     }
+
+    if (semver.valid(semver.coerce(descriptor.range))) {
+      if (semver.lt(semver.coerce(descriptor.range), runtime.version)) {
+        descriptor.range = `^${runtime.version}`
+      }
+    }
+
+    project.topLevelWorkspace.manifest.devDependencies.set(descriptor.identHash, descriptor)
   }
 }
