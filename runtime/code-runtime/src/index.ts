9ef7285c48a246ecd992293ac75d05702b9e0be3
Andrey Linko <AndreyLinko@gmail.com>
2023-05-21 00:22:05 +0300

    feat: runtime export svgr

--- runtime/code-runtime/src/index.ts
+++ runtime/code-runtime/src/index.ts
@@ -1,6 +1,7 @@
 /// <reference path='../types/index.d.ts' />
 
-export * as eslint from './eslint.js'
+export * as svgr from './svgr.js'
 export * as jest from './jest.js'
+export * as eslint from './eslint.js'
 export * as webpack from './webpack.js'
 export * as typescript from './typescript.js'
