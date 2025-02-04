1ec4e58c23818458b6468e99df8adc704708b848
Andrey Linko <AndreyLinko@gmail.com>
2025-02-04 21:39:48 +0300

    feat(common): pack configuration

--- code/code-pack/src/pack.ts
+++ code/code-pack/src/pack.ts
@@ -9,13 +9,31 @@ import { ppath }            from '@yarnpkg/fslib'
 import { getTag }           from './tag.utils.js'
 
 export const pack = async (
-  { workspace, registry, publish, tagPolicy, builder, buildpack }: PackOptions,
+  { workspace, registry, publish, tagPolicy, builder, buildpack, require }: PackOptions,
   context: execUtils.PipevpOptions
 ): Promise<PackOutputs> => {
   const repo = workspace.replace('@', '').replace(/\//g, '-')
   const image = `${registry}${repo}`
 
   const tag = await getTag(tagPolicy)
+
+  const envs = [
+    {
+      name: 'WORKSPACE',
+      value: workspace,
+    },
+    {
+      name: 'CNB_USER_ID',
+      value: '1001',
+    },
+  ]
+
+  if (require && require.length > 0) {
+    envs.push({
+      name: 'BP_REQUIRE',
+      value: require.join(','),
+    })
+  }
 
   const descriptor = {
     project: {
@@ -25,12 +43,7 @@ export const pack = async (
     },
     build: {
       exclude: ['.git', '.yarn/unplugged'],
-      env: [
-        {
-          name: 'WORKSPACE',
-          value: workspace,
-        },
-      ],
+      env: envs
     },
   }
 
